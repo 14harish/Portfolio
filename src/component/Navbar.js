34c80efe9d6909bab3584e89f88bd550be53ff12
14harish <harishpc2003@gmail.com>
2024-07-10 17:03:40 +0530

    new

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -16,12 +16,22 @@ function Navbar() {
   const isActive = (path) => {
     return location.pathname === path;
   };
+  const isActivePortfolio = isActive("/Portfolio") || isActive("/Portfolio/");
+
+const bgColor = isActivePortfolio ? "#FFB400" : "#EEEEEE";
+const darkBgColor = isActivePortfolio ? "#FFB400" : "#2B2A2A";
+
+const className = `group flex items-center justify-start sm:w-11 sm:h-11 w-14 h-14 
+                   bg-${bgColor} dark:bg-${darkBgColor} text-[#666666] dark:text-white 
+                   rounded-full cursor-pointer relative overflow-hidden transition-all 
+                   duration-300 shadow-lg hover:w-32 active:translate-x-1 active:translate-y-1 
+                   hover:bg-[#FFB400] dark:hover:bg-[#FFB400] hover:text-white`;
 
 
   return (
     <div className='sm:fixed flex sm:flex-col sm:gap-4 gap-2 sm:top-64 sm:right-24 sm:w-10 mt-10 ml-10 sm:mt-0 pb-4'>
       <button 
-      className={`group flex items-center justify-start sm:w-11 sm:h-11 w-14 h-14 dark:bg-${isActive("/Portfolio")  ? "[#FFB400]" : "[#2B2A2A]"} bg-${isActive("/Portfolio") ? "[#FFB400]" : "[#EEEEEE]"} dark:bg-${isActive("/Portfolio/")  ? "[#FFB400]" : "[#2B2A2A]"} bg-${isActive("/Portfolio/") ? "[#FFB400]" : "[#EEEEEE]"} text-[#666666] dark:text-white rounded-full cursor-pointer relative overflow-hidden transition-all duration-300 shadow-lg hover:w-32 active:translate-x-1 active:translate-y-1 hover:bg-[#FFB400] dark:hover:bg-[#FFB400] hover:text-white`}
+      className={className}
        onClick={() => { routePage("/Portfolio") }}>
         <div className="flex items-center justify-center w-full transition-all duration-300 group-hover:justify-start group-hover:px-3">
           <MdHome size={24}  />
