c24ba1da78c5add5e0d20229166961c37cd6f0f3
14harish <harishpc2003@gmail.com>
2024-07-10 16:53:35 +0530

    ne

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -21,7 +21,7 @@ function Navbar() {
   return (
     <div className='sm:fixed flex sm:flex-col sm:gap-4 gap-2 sm:top-64 sm:right-24 sm:w-10 mt-10 ml-10 sm:mt-0 pb-4'>
       <button 
-      className={`group flex items-center justify-start sm:w-11 sm:h-11 w-14 h-14 dark:bg-${isActive("/Portfolio/") ? "[#FFB400]" : "[#2B2A2A]"} bg-${isActive("/Portfolio") ? "[#FFB400]" : "[#EEEEEE]"} text-[#666666] dark:text-white rounded-full cursor-pointer relative overflow-hidden transition-all duration-300 shadow-lg hover:w-32 active:translate-x-1 active:translate-y-1 hover:bg-[#FFB400] dark:hover:bg-[#FFB400] hover:text-white`}
+      className={`group flex items-center justify-start sm:w-11 sm:h-11 w-14 h-14 dark:bg-${isActive("/Portfolio/") ? "[#FFB400]" : "[#2B2A2A]"} bg-${isActive("/Portfolio/") ? "[#FFB400]" : "[#EEEEEE]"} text-[#666666] dark:text-white rounded-full cursor-pointer relative overflow-hidden transition-all duration-300 shadow-lg hover:w-32 active:translate-x-1 active:translate-y-1 hover:bg-[#FFB400] dark:hover:bg-[#FFB400] hover:text-white`}
        onClick={() => { routePage("/Portfolio/") }}>
         <div className="flex items-center justify-center w-full transition-all duration-300 group-hover:justify-start group-hover:px-3">
           <MdHome size={24}  />
