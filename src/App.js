8cedbb1faea997e4a3086216eae7b441741b8d25
14harish <harishpc2003@gmail.com>
2024-07-10 15:22:21 +0530

    new

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
      <AnimatePresence mode='wait'>
           
           <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home />} />
+            <Route path="/Portfolio" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/education" element={<Education />} />
